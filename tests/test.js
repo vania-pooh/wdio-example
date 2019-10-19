972f27015ddec4ece4dd523af5e98965686710d4
Boris Osipov <osipov.boris@gmail.com>
2019-10-19 21:44:26 +0300

    Update\clear wdio dependencies. Use puppeteer-core to avoid downloading unnecessary chrome bins. Polish test samples

--- tests/test.js
+++ tests/test.js
@@ -1,11 +1,11 @@
-const assert = require('assert');
+const { expect } = require('chai');
 
 describe('Beaches page', function () {
     it('should have the right title', async function () {
         await browser.url('https://www.beaches.com/');
          const title = await browser.getTitle();
-        //browser.pause(500000);
-        assert.equal(title, 'Beaches Resorts - Family Friendly Caribbean All-Inclusives & Vacations');
+        // browser.pause(5000);
+        expect(title).equal('Beaches Resorts - Family Friendly Caribbean All-Inclusives & Vacations');
         console.log('This is a test ' + title);
     });
 });
